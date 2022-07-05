6fc0a3972e0c1ca44a80eb0827d0a27417f4a136
David Chuvik <dchuvik@gmail.com>
2022-07-05 19:12:49 -0400

    Connected to API to ask user for zip code and display their current Weather forecast

---
+++ CurrentWeatherForecast/Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+
+namespace CurrentWeatherForecast
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your Zip Code?");
+            var zipCode = Console.ReadLine();
+
+            //Console.WriteLine("What is the API Key?");
+            //var API_key = Console.ReadLine();
+
+            var API_key = "04e1b49f8eaa85823821052ace1a653f";
+            var client = new HttpClient();
+            var WeatherURL = ($"https://api.openweathermap.org/data/2.5/weather?zip={zipCode},&appid={API_key}&units=imperial");
+            var Response = client.GetStringAsync(WeatherURL).Result;
+            var tempInFahrenheit = JObject.Parse(Response).GetValue("main").ToString();
+            Console.WriteLine($"The Weather at {zipCode} is '{tempInFahrenheit}'\n");
+
+
+        }
+    }
+}
